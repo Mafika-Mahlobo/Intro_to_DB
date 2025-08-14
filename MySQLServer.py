76339657fc16bc1a87e42f5f9cf4f9264dae25d2
Mafika <rudolphmafika@gmail.com>
2025-08-14 15:53:16 +0200

    Databases, Task1

---
+++ MySQLServer.py
@@ -0,0 +1,32 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+connection = mysql.connector.connect(
+    host = os.getenv("HOST"),
+    user = os.getenv("USER_NAME"),
+    password = os.getenv("PASSWORD")
+)
+
+if connection:
+    print("COnnected to DB server!")
+else:
+    print("Could not connect to BD server!")
+
+cursor = connection.cursor()
+
+query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+
+try:
+    
+    cursor.execute(query)
+    if cursor.warning_count < 1:
+        print("Database successfully created!")
+    else:
+        print("The database already exists")
+
+
+except mysql.connector.errors.DatabaseError as e:
+    print(e)
